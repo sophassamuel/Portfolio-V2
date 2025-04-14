dbcc28693cc4593181ac6e00109f8b1fab05e5de
sophassamuel <sophaswork@gmail.com>
2025-04-14 17:04:16 +0530

    Css fixes

--- src/components/LongCard.jsx
+++ src/components/LongCard.jsx
@@ -5,12 +5,12 @@ import grain from '@/assets/grain.jpg'
 const LongCard = ({ image, title, description }) => {
     return (
         <div
-            className='w-[90%] lg:w-[50%]  flex flex-col items-center sm:flex-row p-4 sm:p-10  gap-10 bg-[#14212b] backdrop-blur-lg hover:bg-gradient-to-r from-[#5d8943] via-[#047f6a] to-[#5d8943]  shadow-md shadow-primary-dark-50 rounded-lg'
+            className='w-[90%] lg:w-[50%]  flex flex-col-reverse items-center sm:flex-row p-4 sm:p-10  gap-10 bg-[#14212b] backdrop-blur-lg hover:bg-gradient-to-r from-[#5d8943] via-[#047f6a] to-[#5d8943]  shadow-md shadow-primary-dark-50 rounded-lg'
         >
 
             {/* Text Container */}
             <div className='flex flex-col w-full sm:w-[60%] gap-5 items-center sm:items-start  text-black dark:text-white'>
-                <h1 className='text-lg font-bold'>
+                <h1 className='text-lg text-center sm:text-left font-bold'>
                     {title}
                 </h1>
                 <p className='text-xs text-center sm:text-left text-gray-400'>
