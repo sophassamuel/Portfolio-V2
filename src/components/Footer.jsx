cb4867771c064d658766dc4a07e608b161592eaa
sophassamuel <sophaswork@gmail.com>
2025-04-14 14:38:57 +0530

    feat: responsiveness to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,14 +7,12 @@ import { FaGithubSquare } from "react-icons/fa";
 const Footer = () => {
     return (
         <div className='w-full flex justify-center'>
-            <div className='w-full flex flex-col sm:w-[90%] py-5 border-t-1 border-gray-700'>
-
-
-                <div className='flex flex-row justify-between'>
+            <div className='w-[90%] flex flex-col sm:w-[90%] py-5 border-t-1 border-gray-700'>
+                <div className='flex flex-col-reverse lg:flex-row items-center gap-4 justify-between'>
                     <div>
                         <p className='text-gray-500 font-bold'>Created with ❤️ by <span className='text-white'>Sophas</span></p>
                     </div>
-                    <div className='flex flex-row text-white gap-5'>
+                    <div className='flex flex-row flex-wrap justify-center text-white items-center gap-5'>
                         <div className="flex items-center  gap-2">
                             <MdEmail className="w-5 h-5 text-gray-500" /> {/* e.g. from Lucide or Heroicons */}
                             <a href="mailto:sophaswork@gmail.com" className="hover:underline">sophaswork@gmail.com</a>
