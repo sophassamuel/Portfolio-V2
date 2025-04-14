612b20a41aaf6f5462071dff8cf8733be0cbb457
sophassamuel <sophaswork@gmail.com>
2025-04-15 01:58:44 +0530

    bug: animation bugs fixed

--- src/components/LongCard.jsx
+++ src/components/LongCard.jsx
@@ -5,11 +5,12 @@ import { fadeIn } from '@/assets/Animations/fadeIn'
 const LongCard = ({ image, title, description}) => {
     return (
         <motion.div
-            variants={fadeIn('up', 0.2, 1)}
+            variants={fadeIn('up', 0.5, 1)}
             initial="hidden"
             whileInView={"show"}
+            whileHover={{scale: 1.02, transition: { duration: 0.3, ease: "easeOut"  }}}
             viewport={{ once: true, amount: 0.05 }}
-            className='w-[90%] lg:w-[50%]  flex flex-col-reverse items-center sm:flex-row p-8 sm:p-10  gap-10 bg-[#14212b] backdrop-blur-lg hover:bg-gradient-to-r from-[#14212b] via-[#1f2f3c] to-[#14212b]  rounded-lg transition-all duration-300 hover:scale-105 hover:rounded-md hover:shadow-[0px_0px_10px_#05df72] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none'
+                className='w-[90%] lg:w-[50%]  flex flex-col-reverse items-center sm:flex-row p-8 sm:p-10  gap-10 bg-[#14212b] backdrop-blur-lg hover:bg-gradient-to-r from-[#14212b] via-[#1f2f3c] to-[#14212b]  rounded-lg  hover:rounded-md hover:shadow-[0px_0px_10px_#05df72] '
         >
 
             {/* Text Container */}
@@ -20,7 +21,7 @@ const LongCard = ({ image, title, description}) => {
                 <p className='text-xs text-center sm:text-left text-gray-400'>
                     {description}
                 </p>
-                <button className='w-32 h-10 rounded-lg mt-5 bg-white text-black transition-all duration-300  hover:rounded-md hover:shadow-[3px_3px_0px_#05df72] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none'>
+                <button className='w-32 h-10 rounded-lg mt-5 bg-white text-black transition-all duration-300  hover:rounded-md hover:shadow-[3px_3px_0px_#05df72] :translate-x-[0px] :translate-y-[0px] :rounded-2xl :shadow-none'>
                     Read More
                 </button>
             </div>
