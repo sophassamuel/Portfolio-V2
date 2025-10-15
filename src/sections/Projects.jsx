da64b955568b8f4ab2e378aef8ce1469a85c9f0f
sophassamuel <sophaswork@gmail.com>
2025-10-15 20:00:26 +0530

    Whisper space added to projects

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -10,46 +10,91 @@ const Projects = () => {
                 main={'A Glimpse into What I’ve Built'}
                 sub={'Showcasing real-world solutions crafted with passion, precision, and purpose.'}
             />
-            <div className='flex justify-center'>
-                <div className='flex flex-row flex-wrap gap-5 w-[90%] justify-center py-10'>
+            <div className="flex justify-center">
+                <div className="flex flex-row flex-wrap gap-5 w-[90%] py-10 justify-center">
                     <TiltedCard
-                        text={'Autostruct AI'}
-                        bubbleText={['ReactJS','ExpressJS','MongoDB','NodeJS','Tailwind CSS','Google OAuth','OpenAI API','NLP','Scaffolding']}
-                        link={'https://github.com/AutoStructAI'}
+                        text="Whisper Space"
+                        bubbleText={[
+                            "ReactJS",
+                            "ExpressJS",
+                            "Supabase",
+                            "NodeJS",
+                            "Tailwind CSS",
+                            "Google OAuth",
+                            "Gemini API",
+                            "NLP",
+                            "React Native",
+                        ]}
+                        link="https://usewhisper.space/"
                     />
                     <TiltedCard
-                        text={'Medicinal Plant identification'}
-                        bubbleText={['Python', 'Jupyter Notebook', 'NumPy', 'Pandas', 'AI/ML', 'Random Forest Classifier', 'Flask', 'HTML', 'CSS']}
-                        link={'https://github.com/sophassamuel/Medicinal-Plant-Identification-using-CNN-and-deployed-using-flask-'}
+                        text="Autostruct AI"
+                        bubbleText={[
+                            "ReactJS",
+                            "ExpressJS",
+                            "MongoDB",
+                            "NodeJS",
+                            "Tailwind CSS",
+                            "Google OAuth",
+                            "OpenAI API",
+                            "NLP",
+                            "Scaffolding",
+                        ]}
+                        link="https://github.com/AutoStructAI"
+                    />
+                    <TiltedCard
+                        text="Medicinal Plant identification"
+                        bubbleText={[
+                            "Python",
+                            "Jupyter Notebook",
+                            "NumPy",
+                            "Pandas",
+                            "AI/ML",
+                            "Random Forest Classifier",
+                            "Flask",
+                            "HTML",
+                            "CSS",
+                        ]}
+                        link="https://github.com/sophassamuel/Medicinal-Plant-Identification-using-CNN-and-deployed-using-flask-"
                     />
 
                     <TiltedCard
-                        text={'Portfolio V2'}
-                        bubbleText={['React', 'Tailwind', 'ShadeCn', 'Framer Motion', 'CSS']}
-                        link={'https://github.com/sophassamuel/Portfolio-V2'}
+                        text="Portfolio V2"
+                        bubbleText={["React", "Tailwind", "ShadeCn", "Framer Motion", "CSS"]}
+                        link="https://github.com/sophassamuel/Portfolio-V2"
                     />
 
                     <TiltedCard
-                        text={'Facial Landmark Detection & Paralysis Identification'}
-                        bubbleText={['Python', 'Jupyter Notebook', 'NumPy', 'OpenCV', 'TensorFlow', 'Keras', 'Flask', 'HTML', 'CSS']}
-                        link={'https://github.com/sophassamuel/Facial-Landmark-Detection-and-Paralysis-Identification'}
+                        text="Facial Landmark Detection & Paralysis Identification"
+                        bubbleText={[
+                            "Python",
+                            "Jupyter Notebook",
+                            "NumPy",
+                            "OpenCV",
+                            "TensorFlow",
+                            "Keras",
+                            "Flask",
+                            "HTML",
+                            "CSS",
+                        ]}
+                        link="https://github.com/sophassamuel/Facial-Landmark-Detection-and-Paralysis-Identification"
                     />
 
                     <TiltedCard
-                        text={'Blog Site with CRUD operations'}
-                        bubbleText={['React', 'Express', 'Node', 'PostgreSQL', 'Sequelize', 'CSS']}
-                        link={'https://github.com/sophassamuel/Blog-Site-ReachJS-PostgreSQL-ExpressJS'}
+                        text="Soft Computing Algorithms"
+                        bubbleText={[
+                            "Python",
+                            "Jupyter Notebook",
+                            "NumPy",
+                            "Pandas",
+                            "Keras",
+                            "TensorFlow",
+                        ]}
+                        link="https://github.com/sophassamuel/Soft-Computing-Algorithms"
                     />
-
-                    <TiltedCard
-                        text={'Soft Computing Algorithms'}
-                        bubbleText={['Python', 'Jupyter Notebook', 'NumPy', 'Pandas', 'Keras', 'TensorFlow']}
-                        link={'https://github.com/sophassamuel/Soft-Computing-Algorithms'}
-                    />
-
-
                 </div>
             </div>
+
         </div>
     )
 }
